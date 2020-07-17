184540fd45e61684804846f5451534c10ff2ef4a
Yannick MIMBE <lucienyannick@gmail.com>
2020-07-17 17:43:20 +0200

    Adding some tests

--- src/test/java/org/tondeuse/entitees/TondeuseTests.java
+++ src/test/java/org/tondeuse/entitees/TondeuseTests.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -38,5 +40,39 @@ public class TondeuseTests {
 		
 		assertEquals("N", tondeuse.getOrientation().name());
 	}
+	
+	
+	@DisplayName("Test de la méthode testDeplacement()")
+	@Test
+	public void testTestDeplacement() {
+		
+		assertThat(tondeuse.testDeplacement(pelouse, tondeuse, Commande.A)).isTrue() ;
+		
+		tondeuse = new Tondeuse(5, 5, Orientation.valueOf("N")) ;
+		assertThat(tondeuse.testDeplacement(pelouse, tondeuse, Commande.A)).isFalse() ;
+	}
+	
+	
+	@DisplayName("Test de la méthode deplacement()")
+	@Test
+	public void testDeplacement() {
+		
+		tondeuse.deplacement(tondeuse)	 ;	
+		assertEquals(3, tondeuse.getY());
+		
+		tondeuse.setOrientation(Orientation.valueOf("E"));
+		tondeuse.deplacement(tondeuse)	 ;
+		assertEquals(2, tondeuse.getX());
 
+		tondeuse.setOrientation(Orientation.valueOf("S"));
+		tondeuse.deplacement(tondeuse)	 ;
+		assertEquals(2, tondeuse.getY());
+		
+		tondeuse.setOrientation(Orientation.valueOf("W"));
+		tondeuse.deplacement(tondeuse)	 ;
+		assertEquals(1, tondeuse.getX());
+	}
+	
+	
+	
 }
